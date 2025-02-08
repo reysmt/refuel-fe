2aa3f9d1135169aeb3d19e9956006662715242d5
Les <45894506+reysmt@users.noreply.github.com>
2025-02-09 00:22:00 +0100

    make the chart fill the screen

--- src/components/RigPriceHistory.vue
+++ src/components/RigPriceHistory.vue
@@ -1,7 +1,7 @@
 <template>
     <Sidebar v-model:visible="isSidebarVisible" header="Storico" position="full">
         <div class="card">
-             <Line :data="chartData" :options="chartOptions" style=""/>
+             <Line :data="chartData" :options="chartOptions" style="width: 100%; height: 100%;"/>
         </div>
     </Sidebar>
 </template>
