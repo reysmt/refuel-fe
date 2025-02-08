3855392ed319cbcd06f10b6acac6b789c311afd8
Les <45894506+reysmt@users.noreply.github.com>
2025-02-08 23:54:33 +0100

    using vue chart js now & optimized dataset & fixed chart bugs

--- src/components/RigPriceHistory.vue
+++ src/components/RigPriceHistory.vue
@@ -1,17 +1,38 @@
 <template>
     <Sidebar v-model:visible="isSidebarVisible" header="Storico" position="full">
         <div class="card">
-            <Chart type="line" :data="chartData" :options="chartOptions" :height="500"/>
+             <Line :data="chartData" :options="chartOptions" style=""/>
         </div>
     </Sidebar>
 </template>
 <script setup>
 import Sidebar from 'primevue/sidebar';
-import Chart from 'primevue/chart';
 </script>
 <script>
 import { ref } from 'vue';
 import {getRigPriceHistoryByRigId} from '@/services/rigsService';
+import { Line } from 'vue-chartjs';
+import zoomPlugin from 'chartjs-plugin-zoom';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js'
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  zoomPlugin
+)
 export default {
     data() {
         return {
@@ -19,7 +40,23 @@ export default {
             currRig: null,
             chartData: ref({ labels: [], datasets: [] }),
             chartOptions: ref(),
-            datasets : []
+            datasets : [],
+            zoomOptions: {
+                zoom: {
+                    pan:{
+                            enabled: true
+                    },
+                    zoom: {
+                        wheel: {
+                            enabled: true,
+                        },
+                        pinch: {
+                            enabled: true
+                        },
+                        mode: 'xy',
+                    }
+                },
+            }
         }
     },
     mounted() {
@@ -27,37 +64,45 @@ export default {
     },
     methods:{
         processData(){
-            let labels = [];
-            let rigTypes = [];
+            let labels = []
+            const dataMap = new Map();
             this.currRig.forEach((rig) => {
                 if(labels.indexOf(rig.comuDate) === -1){
                     labels.push(rig.comuDate);
                 }
-                if(rigTypes.indexOf(rig.rigFuelType.rigFuelTypeDescription) === -1){
-                    rigTypes.push(rig.rigFuelType.rigFuelTypeDescription);
-                    let dataset = {
-                        label: rig.rigFuelType.rigFuelTypeDescription,
+                const key = `${rig.rigFuelType.rigFuelTypeDescription}-${rig.self}`;
+                if(!dataMap.has(key)){
+                    let isSelfLabel = rig.self ? "non servito" : "servito"
+                    dataMap.set(key, {
+                        label: rig.rigFuelType.rigFuelTypeDescription + " " + isSelfLabel,
                         data: [],
                         fill: false,
                         borderColor: '#4bc0c0',
                         tension: 0.4
-                    }
-                    this.datasets.push(dataset);
+                    });
+                    
                 }
+                dataMap.get(key).data.push(rig.price.toFixed(2));
             });
-            this.datasets.forEach((dataset) => {
-                dataset.data = this.currRig.filter((rig) => rig.rigFuelType.rigFuelTypeDescription === dataset.label).map((rig) => rig.price);
-            });
-            console.log(rigTypes)
+            this.datasets = Array.from(dataMap.values())
+            console.log(this.datasets)
+            // console.log(this.datasetInfo)
             this.chartData = {
                 labels: labels,
                 datasets: this.datasets
             };
-            console.log(this.chartData)
+            // console.log(this.chartData)
             this.chartOptions = {
                 responsive: true,
-                maintainAspectRatio: false
-            };
+                maintainAspectRatio: false,
+                plugins: this.zoomOptions
+            }
+        },
+        resetChart(){
+            this.chartData = ref({ labels: [], datasets: [] });
+            this.chartOptions = ref();
+            this.currRig = null;
+            this.datasets = [];
         }
     },
     props: ['isVisible', 'rig', 'token'],	
@@ -67,10 +112,7 @@ export default {
             this.isSidebarVisible = newVal;
             if(newVal){
                 // console.log(this.token)
-                this.chartData = ref({ labels: [], datasets: [] });
-                this.chartOptions = ref();
-                this.datasets = [];
-                this.currRig= null;
+                this.resetChart();
                 this.currRig = await getRigPriceHistoryByRigId(this.rig.rigId, this.token);
                 this.processData();
                 // console.log(this.currRig)
