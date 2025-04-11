c9e8ac4efa3b531519eb5e5df7f6bc7c15cff763
Les <>
2025-04-11 20:43:38 +0200

    prime vue 4 upgrade

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ import './assets/streetView.css'
 // import 'primevue/resources/themes/aura-light-indigo/theme.css'
 // import 'primevue/resources/themes/aura-light-purple/theme.css'
 // import 'primevue/resources/themes/aura-light-teal/theme.css';
-import 'primevue/resources/themes/lara-light-blue/theme.css'
+// import 'primevue/resources/themes/lara-light-blue/theme.css'
 import 'primeicons/primeicons.css'
 
 /* Core CSS required for Ionic components to work properly */
@@ -29,10 +29,38 @@ import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config';
 import ToastService from 'primevue/toastservice';
+import Lara from '@primeuix/themes/lara';
+import { semantic } from '@primeuix/themes/lara/base'
+import { definePreset } from '@primeuix/themes';
+
+const MyPreset = definePreset(Lara, {
+    semantic: {
+        primary: {
+            50: '{blue.50}',
+            100: '{blue.100}',
+            200: '{blue.200}',
+            300: '{blue.300}',
+            400: '{blue.400}',
+            500: '{blue.500}',
+            600: '{blue.600}',
+            700: '{blue.700}',
+            800: '{blue.800}',
+            900: '{blue.900}',
+            950: '{blue.950}'
+        }
+    }
+});
 
 const app = createApp(App)
 app.use(IonicVue)
-app.use(PrimeVue)
+app.use(PrimeVue, {
+    theme: {
+        preset: MyPreset,
+        options: {
+            darkModeSelector: false || 'none',
+        }
+    }
+});
 app.use(ToastService)
 app.use(createPinia())
 app.use(router)
